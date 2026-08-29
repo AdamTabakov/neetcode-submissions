9001839be0d30ab379724be96fcbd23003da96b1
Adam <adamtabakov40@gmail.com>
2026-08-29 19:48:12 -0400

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.py
@@ -0,0 +1,74 @@
+class Solution:
+    def pacificAtlantic(self, heights):
+        n = len(heights)
+        m = len(heights[0])
+
+        # list of directions
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
+        # 2 sets to track
+        pacific = set()
+        atlantic = set()
+
+        # Pacific starts
+        q = deque()
+
+        # add applicable to pacific
+        for j in range(m):
+            q.append((0, j))
+            pacific.add((0, j))
+
+        for i in range(n):
+            q.append((i, 0))
+            pacific.add((i, 0))
+
+        # BFS Pacific
+        while q:
+            x, y = q.popleft()
+
+            for dx, dy in directions:
+                nx = x + dx
+                ny = y + dy
+
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+
+                if (nx, ny) in pacific:
+                    continue
+
+                if heights[nx][ny] >= heights[x][y]:
+                    pacific.add((nx, ny))
+                    q.append((nx, ny))
+
+        # Atlantic starts
+        q = deque()
+
+        # add applicable to atlantic
+        for j in range(m):
+            q.append((n - 1, j))
+            atlantic.add((n - 1, j))
+
+        for i in range(n):
+            q.append((i, m - 1))
+            atlantic.add((i, m - 1))
+
+        # BFS Atlantic
+        while q:
+            x, y = q.popleft()
+
+            for dx, dy in directions:
+                nx = x + dx
+                ny = y + dy
+
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+
+                if (nx, ny) in atlantic:
+                    continue
+
+                if heights[nx][ny] >= heights[x][y]:
+                    atlantic.add((nx, ny))
+                    q.append((nx, ny))
+
+        # Cells reachable from both
+        return list(pacific & atlantic)
